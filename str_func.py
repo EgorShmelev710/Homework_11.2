2acd47d50eb00b51260f97ee239191a1eab4dfaa
Egor Shmelev <shmelevyegor25@gmail.com>
2023-12-19 18:07:49 -0300

    fixes to str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def upperator(value):
+    """функция делает все буквы в строке заглавными"""
     return value.upper()
+
+def titleator(value):
+    """функция делает первые буквы каждого слова заглавными"""
+    return value.title()
